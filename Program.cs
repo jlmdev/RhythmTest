196e8f611b256f3b797ae80bd7c6d4338796db77
Josh Mann <jlmann72@gmail.com>
2020-08-27 18:28:12 -0400

    Experimental Connections

--- Program.cs
+++ Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace RhythmTest
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Band
     {
-      Console.WriteLine("Welcome to C#");
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CountryOfOrigin { get; set; }
+        public int NumberOfMembers { get; set; }
+        public string Website { get; set; }
+        public string Style { get; set; }
+        public bool IsSigned { get; set; }
+        public string ContactName { get; set; }
+        public string ContactPhoneNumber { get; set; }
     }
-  }
+
+    class Album
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsExplicit { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int BandId { get; set; }
+        public Band Band { get; set; }
+    }
+
+    class RhythmTestContext : DbContext
+    {
+        public DbSet<Band> Bands { get; set; }
+        public DbSet<Album> Albums { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("server=localhost;database=SuncoastMovies");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new RhythmTestContext();
+
+            var bands = context.Bands;
+            var albums = context.Albums.Include(album => album.BandId);
+        }
+    }
 }
